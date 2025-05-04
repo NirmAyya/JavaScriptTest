bf11eb6ba50390426e772bb01abec5ffb33b0dd1
NirmAyya <nirmalayyagari@gmail.com>
2025-05-04 14:47:11 -0400

    follower tracker

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -17,3 +17,9 @@ function checkCountValue() {
     alert("Your Instagram post gained 20 followers! Keep it up!");
   }
 }
+
+function resetCount(){
+    count=0;
+    document.getElementById('countDisplay').innerHTML=count;
+    alert("the follower count has been reset to 0");
+}
