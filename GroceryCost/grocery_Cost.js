eea76f67c7ba7a8c89e621ba9fd27796a07df175
NirmAyya <nirmalayyagari@gmail.com>
2025-05-04 13:51:31 -0400

    grocery cost

---
+++ GroceryCost/grocery_Cost.js
@@ -0,0 +1,14 @@
+let item_One, item_Two, item_Three;
+
+
+function calculateCost(){
+    item_One=parseFloat(document.getElementById('item_One').value);
+    item_Two=parseFloat(document.getElementById('item_Two').value);
+    item_Three=parseFloat(document.getElementById('item_Three').value);
+
+    let cost=item_One+item_Two+item_Three;
+
+    document.getElementById('result').innerText = `the cost of your groceries is: ${cost}`;
+
+
+}
